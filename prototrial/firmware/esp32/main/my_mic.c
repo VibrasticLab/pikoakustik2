606140cd641bf0631b6fb0152d0de7e3a7c132cd
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2022-02-15 11:34:38 +0700

    Mic result seems weird

--- prototrial/firmware/esp32/main/my_mic.c
+++ prototrial/firmware/esp32/main/my_mic.c
@@ -9,7 +9,7 @@
 
 #include "my_includes.h"
 
-uint8_t recStatus;
+uint8_t recStatus=false;
 
 static int micRaw(uint16_t* buffRaw){
     esp_err_t errMic;
@@ -36,10 +36,10 @@ static void micTask(void *pvParameter){
     int vread;
     uint16_t i;
     uint16_t rec16[ESP_NOW_MAX_DATA_LEN] = {0};
-    recStatus = 0;
+    recStatus = false;
 
     while(1){
-        if(recStatus==1){
+        if(recStatus){
             vread = micRaw(rec16);
             for(i=0;i<vread;i++){
                 printf("%i,",rec16[i]);
@@ -54,7 +54,7 @@ static int micGet(int argc, char **argv){
     int vread;
     uint16_t i;
     uint16_t rec16[ESP_NOW_MAX_DATA_LEN] = {0};
-    recStatus = 0;
+    recStatus = false;
 
     vread = micRaw(rec16);
     for(i=0;i<vread;i++){
@@ -66,8 +66,8 @@ static int micGet(int argc, char **argv){
 }
 
 static int get_raw(int argc, char **argv){
-    if(recStatus==0){recStatus=1;}
-    else{recStatus=0;}
+    if(!recStatus){recStatus=true;}
+    else{recStatus=false;}
 
     return 0;
 }
@@ -94,14 +94,14 @@ static void micRegister(void){
 void start_mic(void){
     i2s_config_t micConf = {
         .mode = (i2s_mode_t) (I2S_MODE_MASTER|I2S_MODE_RX),
-        .sample_rate = 44100,
+        .sample_rate = 11025, // or 44100 if you like
         .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
         .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
         .communication_format = (i2s_comm_format_t) (I2S_COMM_FORMAT_STAND_I2S | I2S_COMM_FORMAT_STAND_MSB),
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
         .dma_buf_count = 4,
         .dma_buf_len = ESP_NOW_MAX_DATA_LEN * 4,
-        .use_apll = true,
+        .use_apll = false,
         .tx_desc_auto_clear = false,
         .fixed_mclk = 0,
     };
