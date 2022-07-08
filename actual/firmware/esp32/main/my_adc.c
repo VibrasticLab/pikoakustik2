8769c70fab5f512d56897406802cbbae6792f402
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2022-07-08 13:34:48 +0700

    prepare for F303RB

---
+++ actual/firmware/esp32/main/my_adc.c
@@ -0,0 +1,17 @@
+/**
+ * @file my_adc.c
+ * @brief ADC subsystem code
+ *
+ * @addtogroup Battery
+ * @{
+ */
+
+void my_adcInit(void){
+
+}
+
+void my_adcValue(void){
+
+}
+
+ /** @} */
---
+++ actual/firmware/esp32/main/my_adc.h
@@ -0,0 +1,12 @@
+/**
+ * @file my_adc.h
+ * @brief ADC subsystem header
+ *
+ * @addtogroup Battery
+ * @{
+ */
+
+void my_adcInit(void);
+void my_adcValue(void);
+
+ /** @} */
