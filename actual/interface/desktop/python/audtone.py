6bca986f15a2ac65781ea439d9b4a5370b72f002
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2022-11-18 19:37:03 +0700

    Python Dataviewer

--- actual/interface/desktop/python/audtone.py
+++ actual/interface/desktop/python/audtone.py
@@ -7,14 +7,17 @@
 
 # Imports
 import sys
+from pathlib import Path
 from PyQt5.QtWidgets import QApplication,QWidget,QPushButton
 from PyQt5.QtWidgets import QLabel,QFileDialog
 
 # The Main Class
 class AudiometriViewer():
     def __init__(self):
-        self.stt_fName = "Data Open: "
+        self.stt_fOpenData = "Data Open: "
         self.openDataFName = ""
+        self.stt_fOpenCalib = "Calibration Open: "
+        self.openCalibFName = ""
 
         self.app = QApplication(sys.argv)
 
@@ -23,16 +26,30 @@ class AudiometriViewer():
         self.win.setWindowTitle("Audiometri Data Viewer")
 
         self.btn_loadData = QPushButton("Load Data",self.win)
-        self.btn_loadData.setToolTip("Load teh HT*.TXT data record")
+        self.btn_loadData.setToolTip("Load the HT*.TXT data record")
         self.btn_loadData.move(10,10)
         self.btn_loadData.clicked.connect(self.data_open)
 
+        self.btn_loadCalib = QPushButton("Load Calibration",self.win)
+        self.btn_loadCalib.setToolTip("Load the Calibration data")
+        self.btn_loadCalib.move(100,10)
+        self.btn_loadCalib.clicked.connect(self.calib_open)
+
+        self.btn_about = QPushButton("About",self.win)
+        self.btn_about.setToolTip("Show Some Info")
+        self.btn_about.move(205,10)
+
         self.lbl_openData = QLabel(self.win)
-        self.lbl_openData.setText(self.stt_fName)
+        self.lbl_openData.setText(self.stt_fOpenData)
         self.lbl_openData.move(10,60)
 
-        self.lbl_openCalib = QLabel("Data Calibration: ",self.win)
+        self.lbl_openCalib = QLabel(self.win)
+        self.lbl_openCalib.setText(self.stt_fOpenCalib)
         self.lbl_openCalib.move(10,80)
+
+# Shorten File Path for label only
+    def shorten_path(self,file_path,length):
+        return Path(*Path(file_path).parts[-length:])
 
 # Open Data
     def data_open(self):
@@ -41,8 +58,21 @@ class AudiometriViewer():
         fName,_ = QFileDialog.getOpenFileName(None,"Audiometri Data Record","","All Files (*)",options=opts)
         if fName:
             self.openDataFName = fName
-            self.stt_fName = "Data Open: " + fName
-            self.lbl_openData.setText(self.stt_fName)
+            self.stt_fOpenData = "Data Open: %s" % (self.shorten_path(fName,3))
+            self.lbl_openData.setText(self.stt_fOpenData)
+            self.lbl_openData.adjustSize()
+
+# Open Calibration
+    def calib_open(self):
+        opts = QFileDialog.Options()
+        opts |= QFileDialog.DontUseNativeDialog
+        fName,_ = QFileDialog.getOpenFileName(None,"Audiometri Calibration","","All Files (*)",options=opts)
+        if fName:
+            self.openCalibFName = fName
+            self.stt_fOpenCalib = "Calibration Open: %s" % (self.shorten_path(fName,3))
+            self.lbl_openCalib.setText(self.stt_fOpenCalib)
+            self.lbl_openCalib.adjustSize()
+
 
 # Build the GUI
     def gui_run(self):
